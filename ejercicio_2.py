1412e50a773ea22e88adc52fadbad6fdde12e485
Martín Fernández <martinffg@gmail.com>
2025-04-25 21:46:42 -0300

    fix_ejericio2

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -2,14 +2,17 @@
 
 
 def buscarPalabra(palabra, *args):
-    i = 0
-    respuesta = False
+    try:
+        respuesta = False
 
-    print(args)
-    for arg in args:
-        if palabra == args[i]:
+        posicion = args.index(palabra)
+    except ValueError:
+        respuesta = False
+    else:
+        if posicion >= 0:
             respuesta = True
-    return respuesta
+    finally:
+        return respuesta
 
 
 try:
@@ -21,22 +24,19 @@ try:
         "Ingresar una palabra a buscar en la lista. Enter para continuar: \n"
     )
 
-    print(lista_palabras)
-    print(palabra)
+    # print(lista_palabras)
+    # print(palabra)
 
-    # lista_tupla = tuple(lista_palabras)
-
-    cadena_con_comas = "'" + "','".join(lista_palabras) + "'"
-
-    print(cadena_con_comas)
-
-    respuesta = True if buscarPalabra(palabra, cadena_con_comas) else False
+    encontrado = True if buscarPalabra(palabra, *lista_palabras) else False
     print("Procesando números ingresados.")
 except TypeError:
     print(f"Ha ocurrido un error con los tipos de datos")
 except Exception as e:
     print(f"Ha ocurrido un error. {e}")
 else:
-    print(f"La palabra {palabra} está en la lista? {respuesta}")
+    if encontrado:
+        print(f"EXITO!! La palabra SI está en la lista cargada. ")
+    else:
+        print(f"ERROR!! La palabra NO está en la lista cargada. ")
 finally:
     print("Fin del programa.")
